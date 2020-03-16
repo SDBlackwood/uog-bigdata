78e49d560a87bf8d82714280b66efa91407e8db4
Balint Haller <balint.haller@skyscanner.net>
2020-03-16 21:45:22 +0000

    initial logic for mapper

--- src/main/java/MyMapper.java
+++ src/main/java/MyMapper.java
@@ -1,28 +1,71 @@
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import utils.PorterStemmer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-	static enum Counters { NUM_RECORDS, NUM_LINES, NUM_BYTES }
-	private Text _key = new Text();
-	private IntWritable _value = new IntWritable();
+public class MyMapper extends Mapper<LongWritable, Text, CompositeKey, IdCountPair> {
+
+	static enum Counters { NUM_DOCUMENTS, TOTAL_TOKENS }
+	Set<String> stopwords = new HashSet<>();
+	PorterStemmer porterStemmer = new PorterStemmer();
+
+	@Override
+	protected void setup(Context context) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader("stopword-list.txt"));
+		String word;
+
+		while((word = bufferedReader.readLine()) != null) {
+			this.stopwords.add(word);
+		}
+		bufferedReader.close();
+	}
+
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\n");
+		StringTokenizer tokenizer = new StringTokenizer(value.toString());
+
+		Map<CompositeKey, Integer> termCounts = new HashMap<>();
+		int documentLength = 0;
 
 		while (tokenizer.hasMoreTokens()) {
-			String line = tokenizer.nextToken();
-			int sep = line.indexOf(' ');
-			this._key.set((sep == -1) ? line : line.substring(0, line.indexOf(' ')));
-			this._value.set(1);
-			context.write(this._key, this._value);
-			context.getCounter(Counters.NUM_LINES).increment(1);
+			String token = tokenizer.nextToken();
+
+			String processedWord = this.processWord(token);
+			if (processedWord == null) continue;
+
+			CompositeKey termKey = new CompositeKey(processedWord);
+
+			int count = termCounts.getOrDefault(termKey, 0);
+			termCounts.put(termKey, count + 1);
+
+			documentLength++;
+
 		}
-		context.getCounter(Counters.NUM_BYTES).increment(value.getLength());
-		context.getCounter(Counters.NUM_RECORDS).increment(1);
+
+		context.getCounter(Counters.TOTAL_TOKENS).increment(documentLength);
+		context.getCounter(Counters.NUM_DOCUMENTS).increment(1);
+
+		context.write(new CompositeKey(key), new IdCountPair(key, documentLength));
+
+		for (CompositeKey termKey : termCounts.keySet()) {
+			context.write(termKey, new IdCountPair(key, termCounts.get(termKey)));
+		}
+	}
+
+	private String processWord(String word) {
+		if (this.stopwords.contains(word)) {
+			return null;
+		}
+
+		return this.porterStemmer.stem(this.stripWord(word.toLowerCase()));
+	}
+
+	private String stripWord(String word) {
+		return word.replaceAll("[^A-Za-z0-9]", "");
 	}
 }
