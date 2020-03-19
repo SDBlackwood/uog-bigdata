e8c3d47dcb98d53c867550dca7eb5d94148d3e71
Balint Haller <balint.haller@skyscanner.net>
2020-03-19 18:05:03 +0000

    delete unneeded output format class

--- src/main/java/MyOutputFormat.java
+++
@@ -1,9 +0,0 @@
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-
-import java.io.IOException;
-
-public class MyOutputFormat<K, V> extends TextOutputFormat<K, V> {
-
-}
